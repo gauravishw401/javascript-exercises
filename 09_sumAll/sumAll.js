c3dae16d8a0ae677c14630bfb3c89e95cf540682
gaurav vishwakarma <206064760+gauravishw401@users.noreply.github.com>
2025-06-04 20:06:11 +0530

    ALL TESTS PASSED

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,36 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let arr = [];
 
+  if (
+    a < 0 ||
+    b < 0 ||
+    !Number.isInteger(a) ||
+    !Number.isInteger(b) ||
+    typeof a !== "number" ||
+    typeof b !== "number"
+  ) {
+    return "ERROR";
+  } else if (a > b) {
+    for (i = a; i >= b; i--) {
+      arr.push(i);
+    }
+
+    let addedResult = arr.reduce((sum, current) => {
+      return sum + current;
+    });
+
+    return addedResult;
+  } else {
+    for (i = a; i <= b; i++) {
+      arr.push(i);
+    }
+
+    let addedResult = arr.reduce((sum, current) => {
+      return sum + current;
+    });
+
+    return addedResult;
+  }
 };
 
 // Do not edit below this line
