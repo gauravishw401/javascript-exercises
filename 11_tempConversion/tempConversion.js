3a7fd16e721909e05448870ab6e57d23c912fe69
gaurav vishwakarma <206064760+gauravishw401@users.noreply.github.com>
2025-06-04 19:44:34 +0530

    ALL TESTS PASSED

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let celsius = ((fahrenheit - 32) * 5) / 9;
+  let celsiusRoundOff = celsius.toFixed(1);
+  return Number(celsiusRoundOff);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let fahrenheit = (celsius * 9) / 5 + 32;
+  let fahrenheitRoundOff = fahrenheit.toFixed(1);
+  return Number(fahrenheitRoundOff);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
