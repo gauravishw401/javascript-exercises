447b61efde15ce9fcce8adfe718cc6353f1d1d9f
gaurav vishwakarma <206064760+gauravishw401@users.noreply.github.com>
2025-06-05 08:28:45 +0530

    ALL TESTS PASSED

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
-};
+// const removeFromArray = function (arg) {
+//   let parameters = arg;
+// };
+
+function removeFromArray() {
+  let arr = arguments[0];
+
+  function findIndex(arg) {
+    for (let value of arr) {
+      if (value === arg) return arr.indexOf(value);
+    }
+  }
+  for (i = 1; i < arguments.length; i++) {
+    if (arr.includes(arguments[i])) {
+      let gotIndex = findIndex(arguments[i]);
+      // console.log(gotIndex);
+      arr.splice(gotIndex, 1);
+      i--;
+    }
+  }
+
+  return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
